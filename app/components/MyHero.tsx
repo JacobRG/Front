eb73d30544732e2b6d7f6d3ae56f88156b46ded5
JacobRG <jacobgambrell@gmail.com>
2024-06-24 19:53:41 -0400

    Added waitlist and plan pages

--- app/components/MyHero.tsx
+++ app/components/MyHero.tsx
@@ -6,7 +6,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 const navigation = [
   { name: 'Home', href: '#' },
   { name: 'Features', href: '#' },
-  { name: 'Plans', href: '#' },
+  { name: 'Plans', href: 'plans' },
 ]
 
 export default function MyHero() {
@@ -146,7 +146,7 @@ export default function MyHero() {
                   </p>
                   <div className="mt-10 flex items-center gap-x-6">
                     <a
-                      href="#"
+                      href="waitlist"
                       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                       Join the Waitlist
